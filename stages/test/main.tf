ee3c26a1d8a0a062db442c8eb629c29db15dc88a
Andreas <info@andreaslorenz.tech>
2025-04-26 03:44:31 +0200

    Update Stages

--- stages/test/main.tf
+++ stages/test/main.tf
@@ -3,4 +3,13 @@ module "wordpress" {
   project = var.project
   stage   = var.stage
   region = var.region
+  
+  db_instance_type = var.db_instance_type
+  db_name = var.db_name
+  db_username = var.db_password
+  
+  ec2_instance_type = var.ec2_instance_type
+  desired_capacity = var.desired_capacity
+  min_size = var.max_size
+  max_size = var.max_size
 }
