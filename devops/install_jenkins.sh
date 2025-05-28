f122b543ca431b011a86ae25afd762675f0fdbc8
Andreas Lorenz <info@andreaslorenz.tech>
2025-05-28 13:09:16 +0200

    Update Jenkins install script

--- devops/install_jenkins.sh
+++ devops/install_jenkins.sh
@@ -5,6 +5,9 @@ set -e
 echo "Updating system..."
 sudo dnf update -y
 
+echo "Installing Git..."
+sudo yum install git -y
+
 echo "Installing Java 17..."
 sudo dnf install -y java-17-amazon-corretto
 
@@ -17,6 +20,22 @@ sudo systemctl enable --now docker
 echo "Adding users to the docker group..."
 sudo usermod -aG docker ec2-user
 sudo usermod -aG docker jenkins 2>/dev/null || echo "Jenkins user not yet created — will add later."
+
+echo "Installing Helm..."
+sudo curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | sudo bash
+
+echo "Installing Kubectl..."
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.2/2024-11-15/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+
+export ARCH=amd64
+export PLATFORM=$(uname -s)_$ARCH
+
+echo "Installing Eksctl..."
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
 
 echo "Adding Jenkins repo and key..."
 sudo curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
